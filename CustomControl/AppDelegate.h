5f2035868e6865636196c2d31b77a0483f769f62
Evgeniya Mikhailova <mikhailova.evgeniya@gmail.com>
2016-06-07 12:39:36 +0500

    Initial Commit

---
+++ CustomControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomControl
+//
+//  Created by Женя Михайлова on 07.06.16.
+//  Copyright © 2016 mikhailova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomControl
+//
+//  Created by Женя Михайлова on 07.06.16.
+//  Copyright © 2016 mikhailova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
